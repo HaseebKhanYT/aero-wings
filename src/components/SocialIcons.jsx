50b400069f88cfa5efefeb3944c0f5143d7e3d83
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-06-20 16:03:02 +0530

    chore: update social media links

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -16,21 +16,27 @@ export default function SocialIcons() {
   return (
     <>
       <span className="socialIcons">
-        <a className="socialIcon" href="https://facebook.com">
+        <a className="socialIcon" href="https://www.facebook.com/kwaheeduddin">
           <img className="imageMain" src={FBiconLight} alt="fb" />
           <img className=" imageHover" src={FBiconColor} alt="fb" />
         </a>
-        <a className="socialIcon" href="https://instagram.com">
+        <a
+          className="socialIcon"
+          href="https://www.instagram.com/aerowingstravel/"
+        >
           <img className="imageMain" src={InstaiconLight} alt="insta" />
-          <img className=" imageHover" src={InstaiconColor} alt="insta" />
+          <img className="imageHover" src={InstaiconColor} alt="insta" />
         </a>
-        <a className="socialIcon" href="https://linkedin.com">
+        <a
+          className="socialIcon"
+          href="https://linkedin.com/company/aerowings-travel"
+        >
           <img className="imageMain" src={LiconLight} alt="linkedin" />
           <img className="imageHover" src={LiconColor} alt="linkedin" />
         </a>
-        <a className="socialIcon" href="https://whatsapp.com">
+        <a className="socialIcon" href="https://wa.me/919177177011?text=Hello">
           <img className="imageMain" src={WAiconLight} alt="whatsapp" />
-          <img className=" imageHover" src={WAiconColor} alt="whatsapp" />
+          <img className="imageHover" src={WAiconColor} alt="whatsapp" />
         </a>
         <a className="socialIcon" href="mailto:info@aerowings.in">
           <img className="imageMain" src={MAILiconLight} alt="mail" />
