20c57baa6e9a2eb93bf8626157f16d550012988c
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-06-20 09:43:19 +0530

    feat: add train logos and carousels

--- src/views/Train.jsx
+++ src/views/Train.jsx
@@ -1,8 +1,83 @@
+import { EffectFade, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router-dom";
 import trainHeaderImg from "../assets/images/train-header-img.png";
 import whatsappColor from "../assets/services/whatsappColor.svg";
 
-import "../styles/Train.scss"
+import trainLogo1 from "../assets/train/trainLogos01.svg";
+import trainLogo2 from "../assets/train/trainLogos02.svg";
+import trainLogo3 from "../assets/train/trainLogos03.svg";
+import trainLogo4 from "../assets/train/trainLogos04.svg";
+
+import trainImages1 from "../assets/train/trainImages01.svg";
+import trainImages2 from "../assets/train/trainImages02.svg";
+import trainImages3 from "../assets/train/trainImages03.svg";
+import trainImages4 from "../assets/train/trainImages04.svg";
+import trainImages5 from "../assets/train/trainImages05.svg";
+import trainImages6 from "../assets/train/trainImages06.svg";
+import trainImages7 from "../assets/train/trainImages07.svg";
+import trainImages8 from "../assets/train/trainImages08.svg";
+import trainImages9 from "../assets/train/trainImages09.svg";
+
+const trainImages = [
+  {
+    id:1,
+    imageUrl: trainImages1,
+  },
+  {
+    id:2,
+    imageUrl: trainImages2,
+  },
+  {
+    id:3,
+    imageUrl: trainImages3,
+  },
+  {
+    id:4,
+    imageUrl: trainImages4,
+  },
+  {
+    id:5,
+    imageUrl: trainImages5,
+  },
+  {
+    id:6,
+    imageUrl: trainImages6,
+  },
+  {
+    id:7,
+    imageUrl: trainImages7,
+  },
+  {
+    id:8,
+    imageUrl: trainImages8,
+  },
+  {
+    id:9,
+    imageUrl: trainImages9,
+  },
+]
+
+const trainLogos = [
+  {
+    id: 1,
+    imageUrl: trainLogo1,
+  },
+  {
+    id: 2,
+    imageUrl: trainLogo2,
+  },
+  {
+    id: 3,
+    imageUrl: trainLogo3,
+  },
+  {
+    id: 4,
+    imageUrl: trainLogo4,
+  },
+];
+
+import "../styles/Train.scss";
 export default function Train() {
   return (
     <div className="train">
@@ -30,11 +105,46 @@ export default function Train() {
         the state-of-the-art train system in Saudi Arabia that connects the
         cities of Mecca, Medina, and Jeddah.
       </div>
-      <div className="trainCompanyLogos red">
-        train company logos will be here
+      <div className="trainCompanyLogos my-5">
+        <div className="container">
+          <div className="row g-3">
+            {trainLogos.map((logos) => (
+              <div
+                className="col col-lg-3 d-flex justify-content-center align-items-center"
+                key={logos.id}
+              >
+                <div className="logoContainer d-flex justify-content-center">
+                  <img src={logos.imageUrl} width={"175px"} alt="logo img" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="trainCaraousel red">
-        carousel will be here
+      <div className="trainCaraousel my-5">
+      <Swiper
+          effect="fade"
+          fadeEffect={{ crossFade: true }}
+          modules={[Autoplay, EffectFade]}
+          loop={true}
+          slidesPerView={1}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {trainImages.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <img
+                className="trainSlideImage"
+                src={slide.imageUrl}
+                width={"100%"}
+                height={"720px"}
+                alt="slide image"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="buttonContainer my-5">
         <Link to="/contact" className="contactButton">
