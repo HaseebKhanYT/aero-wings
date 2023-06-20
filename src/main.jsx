8b08ec9ff88e76a12792a084c90c5c5a40b21081
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-06-20 09:53:55 +0530

    chore: replace timeline image and refactor bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/js/dist/modal";
-import "bootstrap/js/dist/tab";
-import "bootstrap/js/dist/collapse";
+import "bootstrap/dist/js/bootstrap.min.js";
+// import "bootstrap/js/dist/modal";
+// import "bootstrap/js/dist/tab";
+// import "bootstrap/js/dist/collapse";
 
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
