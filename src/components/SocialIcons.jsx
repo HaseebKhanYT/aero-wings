3ef2b6538b03fff986a7c9b0088e9ac9d6db7197
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-06-20 09:52:00 +0530

    chore: add linkedin logo

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,12 +1,14 @@
 import "../styles/SocialIcons.scss";
 import FBiconColor from "../assets/icons/facebook-color.svg";
 import InstaiconColor from "../assets/icons/instagram-color.svg";
-import TWTiconColor from "../assets/icons/twitter-color.svg";
+
 import WAiconColor from "../assets/icons/whatsapp-color.svg";
 import MAILiconColor from "../assets/icons/mail-color.svg";
+import LiconColor from "../assets/icons/linkedin-color.svg";
+import LiconLight from "../assets/icons/linkedin-light.svg";
 import FBiconLight from "../assets/icons/facebook-light.svg";
 import InstaiconLight from "../assets/icons/instagram-light.svg";
-import TWTiconLight from "../assets/icons/twitter-light.svg";
+
 import WAiconLight from "../assets/icons/whatsapp-light.svg";
 import MAILiconLight from "../assets/icons/mail-light.svg";
 
@@ -15,23 +17,23 @@ export default function SocialIcons() {
     <>
       <span className="socialIcons">
         <a className="socialIcon" href="https://facebook.com">
-          <img loading="lazy" className="imageMain" src={FBiconLight} alt="fb" />
+          <img className="imageMain" src={FBiconLight} alt="fb" />
           <img className=" imageHover" src={FBiconColor} alt="fb" />
         </a>
         <a className="socialIcon" href="https://instagram.com">
-          <img loading="lazy" className="imageMain" src={InstaiconLight} alt="insta" />
+          <img className="imageMain" src={InstaiconLight} alt="insta" />
           <img className=" imageHover" src={InstaiconColor} alt="insta" />
         </a>
-        <a className="socialIcon" href="https://twitter.com">
-          <img loading="lazy" className="imageMain" src={TWTiconLight} alt="twitter" />
-          <img className="imageHover" src={TWTiconColor} alt="twitter" />
+        <a className="socialIcon" href="https://linkedin.com">
+          <img className="imageMain" src={LiconLight} alt="linkedin" />
+          <img className="imageHover" src={LiconColor} alt="linkedin" />
         </a>
         <a className="socialIcon" href="https://whatsapp.com">
-          <img loading="lazy" className="imageMain" src={WAiconLight} alt="whatsapp" />
+          <img className="imageMain" src={WAiconLight} alt="whatsapp" />
           <img className=" imageHover" src={WAiconColor} alt="whatsapp" />
         </a>
         <a className="socialIcon" href="mailto:info@aerowings.in">
-          <img loading="lazy" className="imageMain" src={MAILiconLight} alt="mail" />
+          <img className="imageMain" src={MAILiconLight} alt="mail" />
           <img className="imageHover" src={MAILiconColor} alt="mail" />
         </a>
       </span>
