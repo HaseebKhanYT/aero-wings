b0e3abdec6b5d7df36e4d3a56ede8904fa2fea3c
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-06-21 16:54:16 +0530

    refactor: cleaning code

--- src/views/Loader.jsx
+++ src/views/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
       <div className=" d-flex justify-content-end align-items-center flex-column w-100 loader-background">
         <img className="loader-logo" src={logo} alt="AeroWingsLogo" />
         <div className="loader-gif loadingio-spinner-ellipsis-t9jnxvauyv">
-          <div className="ldio-voxxa58fjp">
+          <div className="ldio-voxxa58fjp position-relative w-100 h-100">
             <div></div>
             <div></div>
             <div></div>
