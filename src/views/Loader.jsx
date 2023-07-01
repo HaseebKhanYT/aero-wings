4acee7ddaa8ad083ef077b1bf4390f12d5cdf5c6
HaseebKhanYT <haseebkhanyt@hotmail.com>
2023-07-02 01:03:26 +0530

    chore: loader optimised

--- src/views/Loader.jsx
+++ src/views/Loader.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 export default function Loader() {
   return (
     <>
-      <div className=" d-flex justify-content-end align-items-center flex-column w-100 loader-background">
+      <div className=" d-flex justify-content-end align-items-center flex-column w-100 h-100 loader-background">
         <img className="loader-logo" src={logo} alt="AeroWingsLogo" />
         <div className="loader-gif loadingio-spinner-ellipsis-t9jnxvauyv">
           <div className="ldio-voxxa58fjp position-relative w-100 h-100">
@@ -19,5 +19,3 @@ export default function Loader() {
     </>
   );
 }
-
-//Ready for production
